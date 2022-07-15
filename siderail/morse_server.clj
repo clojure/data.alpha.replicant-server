0d9e59f372c92ff57c118d3740df22f88222a29d
Alex Miller <alex.miller@cognitect.com>
2022-07-15 17:20:28 -0500

    Add morse client and demo

--- siderail/morse_server.clj
+++ siderail/morse_server.clj
@@ -16,31 +16,37 @@
         cache (server.cache/create-remote-cache cache-builder)]
     cache))
 
+(defmacro ^:private thread
+  [^String name daemon & body]
+  `(doto (Thread. (fn [] ~@body) ~name)
+     (.setDaemon ~daemon)
+     (.start)))
+
 ;; called with *in* and *out* bound to a client
 (defn morse-conn
   [{:keys [rds-cache inspect-queue out-fn]}]
   (.println System/out (str "CONNECT"))
   (let [out *out*]
     ;; Thread to read inspect events and push to client
-    (.start (Thread.
-             #(binding [*out* out
-                        server.reader/*server* rds-cache
-                        *data-readers* (assoc *data-readers* 'l/id #'server.reader/lid-reader)]
-                (loop []
-                  (let [obj (.take ^BlockingQueue inspect-queue)]  ;; .poll with timeout later
-                    (when obj
-                      (out-fn {:tag :inspect
-                               :val (server.spi/remotify obj rds-cache)}))
-                    (recur)))))))
+    (thread "morse-server" true
+            (binding [*out* out
+                      server.reader/*server* rds-cache
+                      *data-readers* (assoc *data-readers* 'l/id #'server.reader/lid-reader)]
+              (loop []
+                (let [obj (.take ^BlockingQueue inspect-queue)]  ;; .poll with timeout later
+                  (when obj
+                    (out-fn {:tag :inspect
+                             :val (server.spi/remotify obj rds-cache)}))
+                  (recur))))))
   ;; Request/response loop
   (binding [*read-eval* false
             server.reader/*server* rds-cache
             *data-readers* (assoc *data-readers* 'l/id #'server.reader/lid-reader)]
     (loop []
       (try
-        (let [o (read *in*)
-              _ (.println System/out (str "READ" (:op o)))
+        (let [[o s] (read+string *in*) 
               {:keys [op rid txn] :as r} o]
+          (.println System/out (str "READ: " s))
           (case op
             :fetch (out-fn {:tag :rds :txn txn :val (server.spi/remotify rid rds-cache)})
             :seq (out-fn {:tag :rds :txn txn :val (server.spi/remotify (seq rid) rds-cache)})
@@ -62,20 +68,28 @@
         inspect-queue (ArrayBlockingQueue. 1024)
         out-lock (Object.)
         out-fn (fn [val]
+                 (.println System/out (str "WRITE: " (pr-str val)))
                  (locking out-lock
                    (prn val)))
         config {:rds-cache rds-cache
                 :inspect-queue inspect-queue
                 :out-fn out-fn}
-        server (server/start-server {:port port,
-                                     :name "morse",
-                                     :accept `morse-conn,
+        server (server/start-server {:port port
+                                     :name "morse"
+                                     :accept `morse-conn
+                                     :server-daemon false
                                      :args [config]})]
+    (.println System/out "WAITING")
     (assoc config :server server)))
 
 (defn inspect
   [server val]
   (.offer ^BlockingQueue (:inspect-queue server) val))
+
+(defn start
+  [_]
+  (let [s (morse-server)]
+    (inspect s (vec (range 55)))))
 
 (comment
   (def s (morse-server))
