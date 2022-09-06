670f52613e5695dfff588f2260815c5c2854961c
Fogus <mefogus@gmail.com>
2022-09-06 13:08:41 -0400

    tweaked protocol docstring

--- src/data/replicant/server/impl/protocols.clj
+++ src/data/replicant/server/impl/protocols.clj
@@ -8,4 +8,4 @@
 
 (defprotocol Cache
   (-object->rid [server obj] "Ensure obj in cache, returns rid.")
-  (-rid->object [server key] "Given rid, return obj or nil."))
+  (-rid->object [server key] "Given rid, return obj or nil if rid not found."))
