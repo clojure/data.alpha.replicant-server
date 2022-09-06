26a9ee50bbb32595bf892a55b725787a192128d9
Fogus <mefogus@gmail.com>
2022-09-06 13:10:45 -0400

    added check for failed lookup of lid and throw on failure.

--- src/data/replicant/server/reader.clj
+++ src/data/replicant/server/reader.clj
@@ -5,9 +5,11 @@
 (defn lid-reader
   "Read '#l/id id' and return the cached object"
   [rid]
-  `(let [val#  (data.replicant.server.spi/rid->object spi/*rds-cache* ~rid)
-         mval# (if (instance? clojure.lang.IObj val#)
-                (with-meta val# {:r/id ~rid})
-                val#)]
+  `(let [val#  (data.replicant.server.spi/rid->object *server* ~rid)
+         mval# (if (not (nil? val#))
+                 (if (instance? clojure.lang.IObj val#)
+                   (with-meta val# {:r/id ~rid})
+                   val#)
+                 (throw (ex-info (str "Remote data structure not found in cache for rid " ~rid)
+                                 {:id ~rid})))]
      mval#))
-
